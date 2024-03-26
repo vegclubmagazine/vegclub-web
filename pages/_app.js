ca9f2a9f82972025ac8c52f92db58275d182010f
bolu Talabi <bolutalabi97@gmail.com>
2024-03-26 22:43:46 +0000

    "adsense script commented"

--- pages/_app.js
+++ pages/_app.js
@@ -8,11 +8,11 @@ function MyApp({Component, pageProps})
 {
     return(
         <Fragment>
-            <Script 
+            {/*<Script 
             async 
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3431599940765829"
             crossOrigin="anonymous"
-            ></Script>
+    ></Script>*/}
             <Component {...pageProps}></Component>
         </Fragment>
     )
