0dfd4f2f408e1d2ba942ebb651f48f67a1cf63d5
bolu Talabi <bolutalabi97@gmail.com>
2024-04-04 01:07:06 +0100

    drawer text adjusted

--- components/Drawer.jsx
+++ components/Drawer.jsx
@@ -109,7 +109,7 @@ const Drawer = ({isOpen, onClose})=>{
                             </ul>*/}
                         </div>
                         <div className=" w-full  mt-[3rem] md:mt-[4rem]">
-                            <ul className="list-none w-full md:grid md:gap-x-8  md:grid-rows-5 md:grid-flow-col text-start font-bold text-[1.44rem]">
+                            <ul className="list-none w-full md:grid md:gap-x-8  md:grid-rows-5 md:grid-flow-col text-start font-semibold text-[1.44rem]">
                                 {Categories?.length && Categories?.map((category, index)=>(
                                     <li className={`py-4 border-black/[.1] ${!index ? "border-b-[1px]" : index === (Categories.length - 1) ? "border-b-[1px] md:border-b-0" : index  % (((parseInt(index/5)+ 1)* 5)-1) ? "border-b-[1px]": "border-b-[1px] md:border-b-0"} ease-[cubic-bezier(.19,1,.22,1)] duration-[.34s] hover:text-black/[.4]`} key={index}><Link href={`/${category?.attributes?.slug}`}>{category?.attributes?.name}</Link></li>
                                     
