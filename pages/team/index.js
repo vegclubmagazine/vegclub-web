115ec5dbd36ed34cbdd028f585820daed127dc87
bolu Talabi <bolutalabi97@gmail.com>
2024-04-18 20:15:02 +0100

    team responsive styling

--- pages/team/index.js
+++ pages/team/index.js
@@ -42,15 +42,17 @@ const Team = ({excerpt,team}) =>
             <div className="xl:grid xl:grid-cols-[2fr_1fr]">
                 <div className="border-black/[.1] xl:border-r-[1px]">
                     {team?.map((member,index)=>(
-                            <div className="flex flex-col md:flex-row border-b-[1px] p-[40px]" key={index}>
-                                <div className="w-full flex flex-row md:block md:min-w-[240px] md:max-w-[240px] md:h-[240px] md:aspect-square overflow-hidden">
+                            <div className="md:flex md:flex-row border-b-[1px] px-[20px] py-[40px] sm:px-[40px]" key={index}>
+                                <div className="inline-block align-middle sm:align-top w-[170px] sm:min-w-[240px] max-w-[240px] sm:h-[240px] aspect-square overflow-hidden">
                                     
-                                    <img src={member?.attributes?.image?.data?.attributes?.url} className="w-[240px] md:w-full h-auto object-cover"/>
-                                    <div className="inline-block md:hidden align-top pl-[20px]">
-                                        <h2 className="text-[1.44rem] lg:text-[1.728rem] font-semibold">{member?.attributes?.name}</h2>
+                                    <img src={member?.attributes?.image?.data?.attributes?.url} className="w-full h-auto object-cover"/>
+                                    
+                                </div>
+                                <div className="inline-block align-middle md:hidden pl-[20px] max-w-[150px] sm:max-w-fit">
+                                        <h2 className="text-[1.2rem] sm:text-[1.44rem] lg:text-[1.728rem] font-semibold">{member?.attributes?.name}</h2>
                                         <p className="mt-3 font-light">{member?.attributes?.pronouns}</p>
                                         <p className="font-semibold mt-3">{member?.attributes?.position}</p>
-                                        <ul className="list-none mt-3 text-black text-[1.728rem]">
+                                        <ul className="list-none mt-3 text-black text-[1.2rem] sm:text-[1.728rem]">
                                             {member?.attributes?.instagram && (<Link href={`${member?.attributes?.instagram}`}><FaInstagram className="inline-block mr-5 transition-all duration-[.34s] ease-[cubic-bezier(.19,1,.22,1)] hover:text-black/[.4]"></FaInstagram></Link>)}
                                             {member?.attributes?.Facebook && (<Link href={`${member?.attributes?.Facebook}`}><FaFacebook className="inline-block mr-5 duration-[.34s] ease-[cubic-bezier(.19,1,.22,1)] hover:text-black/[.4]"></FaFacebook></Link>)}
                                             {member?.attributes?.twitter && (<Link href={`${member?.attributes?.twitter}`}><FaTwitter className="inline-block mr-5 duration-[.34s] ease-[cubic-bezier(.19,1,.22,1)] hover:text-black/[.4]"></FaTwitter></Link>)}
@@ -68,20 +70,23 @@ const Team = ({excerpt,team}) =>
                                         {isMemberAuthor(member?.attributes?.name) && (
                                             <div className="mt-5">
                                                 <Link href={`team/${slugify(member?.attributes?.name)}`}>
-                                                    <p className="inline-block align-middle mr-2 font-semibold uppercase italic duration-[.34s] ease-[cubic-bezier(.19,1,.22,1)] hover:text-black/[.4]">View Articles</p>
+                                                    <p className="inline-block text-[.833rem] sm:text-[1rem] align-middle mr-2 font-semibold uppercase duration-[.34s] ease-[cubic-bezier(.19,1,.22,1)] hover:text-black/[.4]">View Articles</p>
                                                     <div className="inline-block align-middle border-black/[.4] border-t-[1px] border-r-[1px] rotate-[45deg] w-[8px] h-[8px]"></div>
                                                 </Link>
                                             </div>
                                         )}
 
-                                    </div>
                                 </div>
-                                <div className="md:flex-col md:flex md:row md:pl-[20px]">
-                                    <h2 className="hidden md:block text-[1.44rem] lg:text-[1.728rem] font-semibold">{member?.attributes?.name}</h2>
-                                    <p className="hidden md:block mt-3 font-light">{member?.attributes?.pronouns}</p>
-                                    <p className="hidden md:block font-semibold mt-3">{member?.attributes?.position}</p>
+                                <div className="block mt-5 leading-relaxed  md:hidden">
                                     <p className="mt-3">{member?.attributes?.bio}</p>
-                                    <ul className="hidden md:block list-none mt-3 text-black text-[1.728rem]">
+
+                                </div>
+                                <div className="hidden md:flex md:flex-col md:grow md:pl-[20px]">
+                                    <h2 className="text-[1.44rem] lg:text-[1.728rem] font-semibold">{member?.attributes?.name}</h2>
+                                    <p className="mt-3 font-light">{member?.attributes?.pronouns}</p>
+                                    <p className="font-semibold mt-3">{member?.attributes?.position}</p>
+                                    <p className="mt-3">{member?.attributes?.bio}</p>
+                                    <ul className="list-none mt-3 text-black text-[1.728rem]">
                                         {member?.attributes?.instagram && (<Link href={`${member?.attributes?.instagram}`}><FaInstagram className="inline-block mr-5 transition-all duration-[.34s] ease-[cubic-bezier(.19,1,.22,1)] hover:text-black/[.4]"></FaInstagram></Link>)}
                                         {member?.attributes?.Facebook && (<Link href={`${member?.attributes?.Facebook}`}><FaFacebook className="inline-block mr-5 duration-[.34s] ease-[cubic-bezier(.19,1,.22,1)] hover:text-black/[.4]"></FaFacebook></Link>)}
                                         {member?.attributes?.twitter && (<Link href={`${member?.attributes?.twitter}`}><FaTwitter className="inline-block mr-5 duration-[.34s] ease-[cubic-bezier(.19,1,.22,1)] hover:text-black/[.4]"></FaTwitter></Link>)}
@@ -97,13 +102,13 @@ const Team = ({excerpt,team}) =>
                                     </ul>
 
                                     {isMemberAuthor(member?.attributes?.name) && (
-                                    <div className="mt-5 hidden md:block">
-                                        <Link href={`team/${slugify(member?.attributes?.name)}`}>
-                                            <p className="inline-block align-middle mr-2 font-semibold uppercase italic duration-[.34s] ease-[cubic-bezier(.19,1,.22,1)] hover:text-black/[.4]">View Articles</p>
-                                            <div className="inline-block align-middle border-black/[.4] border-t-[1px] border-r-[1px] rotate-[45deg] w-[8px] h-[8px]"></div>
-                                        </Link>
-                                    </div>
-                                )}
+                                        <div className=" mt-5">
+                                            <Link href={`team/${slugify(member?.attributes?.name)}`}>
+                                                <p className="inline-block text-[.833rem] sm:text-[1rem] align-middle mr-2 font-semibold uppercase duration-[.34s] ease-[cubic-bezier(.19,1,.22,1)] hover:text-black/[.4]">View Articles</p>
+                                                <div className="inline-block align-middle border-black/[.4] border-t-[1px] border-r-[1px] rotate-[45deg] w-[8px] h-[8px]"></div>
+                                            </Link>
+                                        </div>
+                                    )}
 
                                 </div>
                                 
