dff4a20c48df02ab7114d22238ca39dc4fe28766
bolu Talabi <bolutalabi97@gmail.com>
2024-04-19 15:26:48 +0100

    ads update

--- components/InHouseAds.jsx
+++ components/InHouseAds.jsx
@@ -6,11 +6,13 @@ const InHouseAds = ({ad})=>
 {
     return (
         <div className="border-black/[.1] py-5 border-b-[1px]">
-            <div className="text-[0.694rem] w-fit mx-auto text-[#CACACA] uppercase">Advertisement</div>
+            <Link href={ad?.attributes?.url} className="w-fit cursor pointer">
+                <div className="text-[0.694rem] w-fit mx-auto text-[#CACACA] uppercase">Advertisement</div>
 
-            <div className="mt-3 relative w-fit mx-auto  my-3 bg-[#cacaca]">
-                <img src={ad?.attributes?.image?.data?.attributes?.url}/>
-            </div>
+                <div className="mt-3 relative w-fit mx-auto  my-3 bg-[#cacaca]">
+                    <img src={ad?.attributes?.image?.data?.attributes?.url}/>
+                </div>
+            </Link>
         </div>
     )
 
