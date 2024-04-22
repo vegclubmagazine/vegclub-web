8360e53763c47dbceb43c7d10e306c5df48efbad
bolu Talabi <bolutalabi97@gmail.com>
2024-04-22 20:55:49 +0100

    adjusted ad sizing

--- components/InHouseAds.jsx
+++ components/InHouseAds.jsx
@@ -5,12 +5,13 @@ import Link from "next/link";
 const InHouseAds = ({ad})=>
 {
     return (
-        <div className="border-black/[.1] py-5 border-b-[1px]">
-            <Link href={ad?.attributes?.url} className="w-fit cursor pointer">
-                <div className="text-[0.694rem] w-fit mx-auto text-[#CACACA] uppercase">Advertisement</div>
+        <div className="border-black/[.1] border-b-[1px]">
+            <Link href={ad?.attributes?.url} className="w-full cursor pointer">
+                {/*<div className="text-[0.694rem] w-fit mx-auto text-[#CACACA] uppercase">Advertisement</div>*/}
 
-                <div className={`mt-3 relative ${ad?.attributes?.size === "small" ? "w-[240px]": ad?.attributes?.size === "medium" ? "w-[340px]": ad?.attributes?.size === "large"? "w-[540px]":"w-[240px]"} mx-auto  my-3 bg-[#cacaca]`}>
+                <div className={`relative w-full mx-auto overflow-hidden   bg-[#cacaca]`}>
                     <img className="w-fit h-auto object-cover" src={ad?.attributes?.image?.data?.attributes?.url}/>
+                    <div className="absolute right-3 bottom-3 px-3 bg-black text-white text-[0.833rem] font-semibold uppercase">Advertisment</div>
                 </div>
             </Link>
         </div>
