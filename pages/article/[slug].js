1e50b76c5773120943839d84b8d7cd83cfbe9cda
bolu Talabi <bolutalabi97@gmail.com>
2024-05-05 16:50:52 +0100

    general update

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -166,7 +166,7 @@ const Article = ({article, articles}) =>
                             When Golden was a young curator in the nineties, her shows, centering Black artists, were unprecedented. Today, those artists are the stars of the art market.
                         </p>*/}
                         <div className="mt-[3rem] article-preview leading-[1.8] mb-5 text-[1.2rem]">
-                            {parse(article?.attributes?.content)}
+                            {article?.attributes?.content && (parse(article?.attributes?.content))}
                         </div>
                     </section>
                    
