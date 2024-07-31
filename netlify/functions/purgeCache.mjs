4e1d00c77d762d74b44e8078ec91bddd71afc6da
bolu Talabi <bolutalabi97@gmail.com>
2024-07-31 16:34:40 +0100

    cache handle via serverless function

---
+++ netlify/functions/purgeCache.mjs
@@ -0,0 +1,12 @@
+// purge all objects across all deploys for this site
+
+import { purgeCache } from "@netlify/functions";
+
+export default async () => {
+
+  console.log("Purging everything");
+
+  await purgeCache();
+
+  return new Response("Purged!", { status: 202 })
+};
